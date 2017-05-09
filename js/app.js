14d3b62b8f279c5b5b09934982397f0edef18088
jason welsh <jasonbwelsh@gmail.com>
2017-05-09 12:51:43 -0400

    JavaScript behavior programmed
    Programmed the highlighted captions and ability to click a caption and
    jump to the related video time

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const video = document.getElementById('my_video');
+const captionSection = document.querySelector('.caption-section');
+const captions = document.querySelectorAll('section span');
+
+// Highlight video caption text
+video.addEventListener('timeupdate', (e) => {
+	for (let i = 0; i < captions.length; i++) {
+		let cap = captions[i];
+		let time = video.getCurrentTime();
+		let captionStart = cap.getAttribute('data-start');
+		let captionEnd = cap.getAttribute('data-end');
+		if ( time >= captionStart && time <= captionEnd ) {
+			cap.className = 'highlight';
+		} else {
+			cap.className = '';
+		}
+	}	
+});
+
+// Click caption text to jump to video time
+captionSection.addEventListener('click', (e) => {
+	let currentTime = e.target.getAttribute('data-start');
+	if (currentTime) {
+		video.setCurrentTime(currentTime);
+	}
+});
